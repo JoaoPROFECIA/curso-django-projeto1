0336e2ff016784a9a6368788de9748bb113287ee
Joao Victor <101950141+JoaoPROFECIA@users.noreply.github.com>
2022-09-22 18:26:28 -0300

    Templates e renderização de HTML no Django

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # A gente também tem apps criados por nós
+    'recipes',
 ]
 
 MIDDLEWARE = [
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('home 1')
-    # return HTTP Response
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
