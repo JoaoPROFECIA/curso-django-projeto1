9b2495a6686d00c1b46a6d0b7a6df6aad21f8ba5
Joao Victor <victorjv3288@gmail.com>
2022-09-23 19:52:18 -0300

    Config
    Configuração para desenvolver no Notebook

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\victo\OneDrive - UNIP\Documentos\GitHub\curso-django-projeto1\venv
+set VIRTUAL_ENV=C:\Users\victo\Documentos\MeusProjetos\curso-django-projeto1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
