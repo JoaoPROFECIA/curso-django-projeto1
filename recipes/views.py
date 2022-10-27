15ced40152e5899ad0faaebe945e8b35fbcca901
Joao Victor <victorjv3288@gmail.com>
2022-10-27 15:22:44 -0300

    Uma nova URL para uma nova View
    Sobre URLs no Django

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'João Victor',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'João Victor',
+    })
